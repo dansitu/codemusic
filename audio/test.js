7b340231cc6a67a788f80bb59ce1c9191004f72c
Ryan Brown <ryan@ryanleebrown.com>
2013-02-16 19:21:29 -0800

    intelligent note scheduling

--- audio/test.js
+++ audio/test.js
@@ -44,36 +44,92 @@ var melodyChannel = 0;
 var drumChannel = 1;
 var sequenceIndex = 0;
 
-var playMelodyNote = function(depth) {
-  MIDI.noteOn(melodyChannel, startNote - depth, velocity, sequenceIndex * sequenceInterval);
-  MIDI.noteOff(melodyChannel, startNote - depth, sequenceIndex * sequenceInterval + noteLength);
-  sequenceIndex++;
-};
-
-var playMunch = function(munch, depth) {
-  playMelodyNote(depth);
-  if (munch.children) {
-    playMunchPile(munch.children, depth + 1);
-  }
-  if (munch.type === 'function' && sequenceIndex % notesPerBar != 0) {
-    sequenceIndex += notesPerBar - sequenceIndex % notesPerBar;
-  }
-};
-
-var playMunchPile = function(pile, depth) {
-  for (var i = 0; i < pile.length; i++) {
-    playMunch(pile[i], depth);
-  }
-};
-
-var playDrums = function() {
+var synthesizeDrums = function() {
+  var midi = [];
   var bars = sequenceIndex / notesPerBar;
   for (var i = 0; i < bars; i++) {
     var index = i * notesPerBar;
     for (var j = 0; j < beatsPerBar; j++) {
-      MIDI.noteOn(drumChannel, 61, velocity, index * sequenceInterval + j * beatLength);
-      MIDI.noteOff(drumChannel, 61, velocity, index * sequenceInterval + (j+1) * beatLength);
+      midi.push({
+        channel: drumChannel,
+        note: 61,
+        velocity: velocity,
+        delay: index * sequenceInterval + j * beatLength,
+        length: beatLength,
+      });
     }
+  }
+  return midi;
+};
+
+var append = function(a, b) {
+  for (var i = 0; i < b.length; i++) {
+    a.push(b[i]);
   }
 };
 
+var synthesizeMunch = function(munch, depth) {
+  var midi = [
+    {
+      channel: melodyChannel,
+      note: startNote - depth,
+      velocity: velocity,
+      delay: sequenceIndex * sequenceInterval,
+      length: noteLength,
+    },
+  ];
+  sequenceIndex++;
+  if (munch.children) {
+    append(midi, synthesizeDepth(munch.children, depth + 1));
+  }
+  if (munch.type === 'function' && sequenceIndex % notesPerBar != 0) {
+    sequenceIndex += notesPerBar - sequenceIndex % notesPerBar;
+  }
+  return midi;
+};
+
+var synthesizeDepth = function(pile, depth) {
+  var midi = [];
+  for (var i = 0; i < pile.length; i++) {
+    append(midi, synthesizeMunch(pile[i], depth));
+  }
+  return midi;
+};
+
+var synthesize = function(pile) {
+  return synthesizeDepth(pile, 0);
+};
+
+var notesPerSchedule = 20;
+var scheduleInterval = 1000;
+
+var play = function(midi) {
+  midi.sort(function(a, b) {
+    return a.delay < b.delay;
+  });
+  var playNext = function(midi, a, b, f) {
+    for (var i = a; i < midi.length && i < b; i++) {
+      var msg = midi[i];
+      MIDI.noteOn(msg.channel, msg.note, msg.velocity, msg.delay);
+      MIDI.noteOff(msg.channel, msg.note, msg.delay + msg.length);
+    }
+    setTimeout(function() { f(midi, b, b + notesPerSchedule, f); }, scheduleInterval);
+  };
+  playNext(midi, 0, notesPerSchedule, playNext);
+};
+
+/*
+MIDI.loadPlugin({
+  api: 'webmidi',
+  soundfontUrl: './MIDI.js-master/soundfont/',
+  instruments: ['acoustic_grand_piano', 'synth_drum'],
+  callback: function() {
+    MIDI.setVolume(0, 127);
+    MIDI.setVolume(1, 127);
+    MIDI.programChange(1, 118);
+    var midi = synthesize(pile);
+    append(midi, synthesizeDrums());
+    play(midi);
+  },
+});
+*/
