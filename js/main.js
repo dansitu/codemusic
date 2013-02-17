dac1c3362a440c28e9ed2f0de5e30e42c04604e8
Daniel Imrie-Situnayake <dansmailbag@googlemail.com>
2013-02-16 18:43:50 -0800

    More refactoring

--- js/main.js
+++ js/main.js
@@ -1,52 +1,63 @@
 $(function(){
 
-  var muncher = new CodeMuncher()
+  // The fileReader reads files for us
+  var fileReader = new FileReader();
+  fileReader.onload = _fileLoaded;
 
-  $("#go").click(function(){
+  // The muncher is what 'digests' our code
+  var muncher = new CodeMuncher();
+
+  // If the 'go' button is clicked, read the file
+  $("#go").click(readSelectedFile)
+    
+  // ## readSelectedFile
+  // Reads the file currently highlighted in the file browser
+  function readSelectedFile(){
   
     var selectedFile = $("#fileBrowser").get(0).files[0];
-    var reader = new FileReader();
 
-    reader.onload = function(evt){
-      muncher.munch(evt.target.result);
+    fileReader.readAsText(selectedFile);
 
-      var munchTree = [];
-      var munches = 0;
-      for(var i=muncher.munchPile.length-1;i>=0;i--){
-        var curr = muncher.munchPile[i];
-        // Put depth1 in output
-        if(curr.depth === 1) {
-          munchTree.splice(0, 0, curr);
-          continue;
-        }
-        // Find the nearest thing with a lower depth
-        // and add this as a child
-        for(var x=i;x>=0;x--){
-          if(muncher.munchPile[x].depth < curr.depth){
-            muncher.munchPile[x].children.splice(0, 0, curr);
-            break;
-          }
+  }
+
+  // ## _fileLoaded
+  // When the fileReader has loaded a file, munch and play it!
+  function _fileLoaded(evt){
+
+    muncher.munch(evt.target.result);
+
+    var munchTree = [];
+    var munches = 0;
+    for(var i=muncher.munchPile.length-1;i>=0;i--){
+      var curr = muncher.munchPile[i];
+      // Put depth1 in output
+      if(curr.depth === 1) {
+        munchTree.splice(0, 0, curr);
+        continue;
+      }
+      // Find the nearest thing with a lower depth
+      // and add this as a child
+      for(var x=i;x>=0;x--){
+        if(muncher.munchPile[x].depth < curr.depth){
+          muncher.munchPile[x].children.splice(0, 0, curr);
+          break;
         }
       }
-
-      MIDI.loadPlugin({
-        api: 'webmidi',
-        soundfontUrl: './audio/MIDI.js-master/soundfont/',
-        instruments: ['acoustic_grand_piano', 'synth_drum'],
-        callback: function() {
-          MIDI.setVolume(0, 127);
-          MIDI.setVolume(1, 127);
-          MIDI.programChange(1, 118);
-          playMunchPile(munchTree, 0);
-          playDrums();
-        },
-      });
-
     }
 
-    reader.readAsText(selectedFile);
-
-  });
+    MIDI.loadPlugin({
+      api: 'webmidi',
+      soundfontUrl: './audio/MIDI.js-master/soundfont/',
+      instruments: ['acoustic_grand_piano', 'synth_drum'],
+      callback: function() {
+        MIDI.setVolume(0, 127);
+        MIDI.setVolume(1, 127);
+        MIDI.programChange(1, 118);
+        playMunchPile(munchTree, 0);
+        playDrums();
+      },
+    });
+  }
 
 });
 
